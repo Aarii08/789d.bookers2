70ab55aba43a692da3a0757ba8c2d248a1945ec0
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-09-03 02:00:13 +0000

    Start

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,17 @@
+class SearchesController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def search
+    @model = params[:model]
+    @word = params[:word]
+    @search = params[:search]
+
+    if @model == "user"
+      @records = User.search_for(@word, @method)
+    else
+      @records = Book.search_for(@word, @method)
+    end
+  end
+
+end
---
+++ app/models/book.rb
@@ -0,0 +1,29 @@
+class Book < ApplicationRecord
+
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
+
+  def self.search_for(word, method)
+    if method == 'perfect'
+      Book.where(title: word)
+    elsif method == 'forward'
+      Book.where('title LIKE ?', word + '%' )
+    elsif method == 'backward'
+      Book.where('title LIKE ?', '%' + word )
+    else
+      Book.where('title LIKE ?', '%' + word + '%' )
+    end
+  end
+
+
+end
