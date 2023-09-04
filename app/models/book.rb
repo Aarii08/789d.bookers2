ff157dfee6b8364ee1f830db4cc173345f6a8d1a
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-09-04 01:57:06 +0000

    8d_Finish

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,6 +8,11 @@ class Book < ApplicationRecord
   validates :body,presence:true,length:{maximum:200}
 
 
+  scope :latest, -> {order(created_at: :desc)}
+  scope :old, -> {order(created_at: :asc)}
+  scope :star_count, -> {order(star: :desc)}
+
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
